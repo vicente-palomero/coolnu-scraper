11b84120e61639e42cc045c2ab0c70f28299780d
vicente palomero <vicente.palomero.lopez@gmail.com>
2021-03-30 19:22:25 +0200

    Fix scripts

--- wikitables.py
+++ wikitables.py
@@ -27,7 +27,10 @@ from urllib.request import urlopen
 #
 # Si se guarda como .csv se puede importar desde Google Spreadsheet sin problemas
 
-def main(argv):
+def run_by_url(url, prize, year, winner, title, info):
+    read_tables(url)
+
+def main(argv, prize, year, winner, title, info):
     url = ''
     outputfile = ''
     try:
@@ -44,24 +47,19 @@ def main(argv):
         else:
             assert False, "unhandled option"
 
-    read_tables(url)
+    read_tables(url, prize, year, winner, title, info)
 
-def read_tables(url):
+def read_tables(url, prize, year, winner, title, info):
     bsObj = urlToBSoup(url)
     tables = bsObj.find_all('table', {'class': 'wikitable'})
     pos = 0
     for t in tables:
         pos = pos + 1
-        table = read_table(t, str(pos))
+        table = read_table(t, str(pos), prize, year, winner, title, info)
         for r in table['body']:
             print(','.join(r))
 
-def read_table(table, position):
-    prize = "Breve"
-    yearTablePosition = 0
-    winnerTablePosition = 3
-    titleTablePosition = 2
-    whereInfoShouldBePosition = 1 # Esto es por si hay una fila desierta
+def read_table(table, position, prize, year, winner, title, info):
 
     head_body = {'head': [], 'body': []}
     for tr in table.select('tr'):
@@ -69,24 +67,24 @@ def read_table(table, position):
             head_body['head'] += [th.get_text().rstrip('\n') for th in tr.find_all(recursive=False)]
         else:
             row = [td for td in tr.find_all(recursive=False)]
-            info = row[whereInfoShouldBePosition].get_text()
-            shallContinue = 'No convocado' in info or 'Desierto' in info # Aquí cuando no se entregó el premio, por lo que sea. Cambiar para adaptar a textos "inválidos" de la tabla
+            info_value = row[info].get_text()
+            shallContinue = 'No convocado' in info_value or 'Desierto' in info_value # Aquí cuando no se entregó el premio, por lo que sea. Cambiar para adaptar a textos "inválidos" de la tabla
             if shallContinue:
                 continue
             pos = [position]
             # pos = [position, position, position] # Para ejemplos en los que hay celdas con hasta 3 filas
-            oneYear = row[yearTablePosition].get_text().strip('\n')
-            year = [oneYear]
+            oneYear = row[year].get_text().strip('\n')
+            year1 = [oneYear]
             # year = [oneYear, oneYear, oneYear] # Para ejemplos en los que hay celdas con hasta 3 filas
-            winners1 = [a.get_text().strip('\n') for a in row[winnerTablePosition].find_all('a')]
+            winners1 = [a.get_text().strip('\n') for a in row[winner].find_all('a')]
             flags = ["-", "-", "-"] #[span for span in row[2].find('span')] # No sé sacar bien el nombre del país
-            titles1 = [i.text for i in row[titleTablePosition].findAll('i')]
-            bodyRow1 = [list(a) for a in zip(year, [prize], "1", winners1, titles1)]
+            titles1 = [i.text for i in row[title].findAll('i')]
+            bodyRow1 = [list(a) for a in zip(year1, [prize], "1", winners1, titles1)]
             head_body['body'] += bodyRow1
             # Adaptado para la tabla del Nadal, marcado como FALSE para que no moleste
             if False and len(row) > 4:
-                winners2 = [a.get_text().strip('\n') for a in row[winnerTablePosition + 2].find_all('a')]
-                titles2 = [i.text for i in row[titleTablePosition + 2].findAll('i')]
+                winners2 = [a.get_text().strip('\n') for a in row[winner + 2].find_all('a')]
+                titles2 = [i.text for i in row[title+ 2].findAll('i')]
                 bodyRow2 = [list(a) for a in zip(year, [prize], "2", winners2, titles2)]
                 head_body['body'] += bodyRow2
     return head_body
@@ -96,4 +94,10 @@ def urlToBSoup(url):
 
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    prize = "Breve" # Nombre en clave del premio
+    year = 0 # Columna donde esta el anyo, empezando por 0
+    winner = 2 # Columna donde esta el ganador, empezando por 0
+    title = 1 # Columnad donde esta el titulo, empezando por 0
+    info = 1 # Esto es por si hay una fila desierta
+    #run_by_url("https://es.wikipedia.org/wiki/Premio_Biblioteca_Breve", prize, year, winner, title, info)
+    main(sys.argv[1:], prize, year, winner, title, info)
